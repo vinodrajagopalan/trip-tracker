40879f102d5f0531ad867e71132883a3f455da75
vinoth <vinothkumar.prof@gmail.com>
2022-03-23 19:06:42 +0530

    Vinoth: Test commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { hot } from "react-hot-loader/root";
 
 class App extends React.Component {
   render() {
-    const { name } = this.props;
+    const { name } = this.props
     return (
       <>
         <h1>Hello world1 {name}</h1>
       </>
-    );
+    )
   }
 }
 
