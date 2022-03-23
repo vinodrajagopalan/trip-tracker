be267a1efb324d1c9da22051d1395d4ff7ba3d47
vinoth <vinothkumar.prof@gmail.com>
2022-03-23 19:14:49 +0530

    VInoth: Test commit for lint fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import Proptypes from "prop-types";
 import { hot } from "react-hot-loader/root";
 
 class App extends React.Component {
   render() {
-    const { name } = this.props
+    const { name } = this.props;
     return (
       <>
         <h1>Hello world1 {name}</h1>
       </>
-    )
+    );
   }
 }
+
+App.propTypes = {
+  name: Proptypes.string.isRequired,
+};
 
 export default hot(App);
