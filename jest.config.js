fe20d87365711912bb9816d07985d0416fb0cbd6
vinoth <vinothkumar.prof@gmail.com>
2022-03-25 17:15:48 +0530

    Vinoth: Added unit test coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    testEnvironment: "jsdom"
+    testEnvironment: "jsdom",
+    coverageThreshold: {
+        global: {
+          statements: 95,
+          branches: 95,
+          functions: 95,
+          lines: 95,
+        },
+      },
+      coverageReporters: ["lcov", "text", "text-summary"]
 };
